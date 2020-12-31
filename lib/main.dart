007f62f3b9c544b27d9e389138833a741c6e24bf
numoonchld <raghavendra.saralaya@gmail.com>
2020-12-31 10:18:35 +0530

    end of chap 6

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
+          backgroundColor: Colors.red[300],
         ),
         body: Center(
-          child: Text('Body Area'),
+          child: Text(
+            'Body Area',
+            style: TextStyle(
+              color: Colors.grey[300],
+              fontSize: 20.0,
+              letterSpacing: 2.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'IndieFlower',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text('Click'),
+          backgroundColor: Colors.red[300],
         ),
       ),
     ));
