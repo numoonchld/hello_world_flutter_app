cd5d524eeeaa7cc2fd283b337cc5f00356aca91c
numoonchld <raghavendra.saralaya@gmail.com>
2020-12-31 16:00:59 +0530

    end of video 7

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A new Flutter project.
 
 A follow along project for the tutorial series [Flutter Tutorial for Beginners](https://www.youtube.com/playlist?list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ)
 
+### Font Installation
+
+- for testing purposes, make sure font is installed in OS 
+    - in addition to copying it to `fonts` folder 
+    - and declaring it in `pubspec.yaml`
--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       // home: Text("Hello World"),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first app'),
-          backgroundColor: Colors.red[300],
-        ),
-        body: Center(
-          child: Text(
-            'Body Area',
-            style: TextStyle(
-              color: Colors.grey[300],
-              fontSize: 20.0,
-              letterSpacing: 2.0,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'IndieFlower',
-            ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        backgroundColor: Colors.red[300],
+      ),
+      body: Center(
+        child: Text(
+          'Body Area',
+          style: TextStyle(
+            color: Colors.grey[900],
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'IndieFlower',
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.red[300],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[300],
+      ),
+    );
+  }
+}
