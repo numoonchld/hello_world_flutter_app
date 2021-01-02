bde763bd7f097464a36a2783e79bef3b1f3b500f
numoonchld <raghavendra.saralaya@gmail.com>
2021-01-03 00:08:54 +0530

    end of video 10

--- lib/main.dart
+++ lib/main.dart
@@ -13,57 +13,20 @@ class Home extends StatelessWidget {
         title: Text('My first app'),
         backgroundColor: Colors.red[300],
       ),
-      body: Center(
-        child: IconButton(
-          icon: Icon(
-            Icons.alternate_email,
-          ),
-          onPressed: () {
-            print('Email Sent!');
-          },
-          color: Colors.amberAccent,
-          iconSize: 50.00,
-        ),
-        // child: RaisedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.mail,
-        //     size: 40.00,
-        //   ),
-        //   label: Text('Send Mail'),
-        // ),
-        // child: FlatButton(
-        //   onPressed: () {
-        //     print('Button Clicked');
-        //   },
-        //   child: Text('Click Me'),
-        //   color: Colors.lightBlue,
-        //   textColor: Colors.white,
-        // ),
-        // child: RaisedButton(
-        //   onPressed: () {
-        //     print('Button Clicked');
-        //   },
-        //   child: Text('Click Me'),
-        //   color: Colors.lightBlue,
-        //   textColor: Colors.white,
-        // ),
-        // child: Icon(
-        //   Icons.ac_unit_rounded,
-        //   color: Colors.blue,
-        //   size: 100.0,
-        // ),
-        // child: Image.network(
-        //     'https://images.unsplash.com/photo-1609105719019-a9e385165907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=935&q=80'),
-        // child: Image.asset('assets/station-1.jpeg'),
-        // Image(
-        //   image: AssetImage('assets/porshe-1.jpeg'),
-        // ),
-        // Image(
-        //   image: NetworkImage(
-        //       'https://pbs.twimg.com/media/Eqd5Vl-XYAAZAJu?format=jpg&name=large'),
-        // ),
+      body: Padding(
+        padding: EdgeInsets.all(30.00),
+        child: Text("Only Padding Widget"),
       ),
+      // body: Container(
+      //   color: Colors.grey[400],
+      //   child: Text('Hello World!'),
+      //   // padding: EdgeInsets.all(20.00),
+      //   padding: EdgeInsets.symmetric(
+      //     horizontal: 20.00,
+      //     vertical: 40.00,
+      //   ),
+      //   margin: EdgeInsets.all(30.00),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
