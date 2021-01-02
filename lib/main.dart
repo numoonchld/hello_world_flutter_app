8005e64509ce242f242b50b98f1c14913151286e
numoonchld <raghavendra.saralaya@gmail.com>
2021-01-02 12:50:13 +0530

    image shortcuts

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,15 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[300],
       ),
       body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://pbs.twimg.com/media/Eqd5Vl-XYAAZAJu?format=jpg&name=large'),
-        ),
+        child: Image.network(
+            'https://images.unsplash.com/photo-1609105719019-a9e385165907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=935&q=80'),
+        // child: Image.asset('assets/station-1.jpeg'),
+        // Image(
+        //   image: AssetImage('assets/porshe-1.jpeg'),
+        // ),
+        //   image: NetworkImage(
+        //       'https://pbs.twimg.com/media/Eqd5Vl-XYAAZAJu?format=jpg&name=large'),
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
